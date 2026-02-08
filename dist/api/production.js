4d7345fa35d7dde2164c23f940a58569e45c0d6c
Benjamin Thio <74221883+BenjaminThio@users.noreply.github.com>
2026-02-09 01:20:01 +0800

    Deleted dist folder.

--- dist/api/production.js
+++
@@ -1,3 +0,0 @@
-import { webhookCallback } from "grammy";
-import { bot } from "../src/bot.js";
-export default async (req, res) => webhookCallback(bot, "http")(req, res);
--- dist/src/birthday-reminder/database.js
+++
@@ -1,6 +0,0 @@
-import { collection, addDoc } from "firebase/firestore";
-import db from "../../utils/firebase-config.js";
-export const birthdayRef = collection(db, "birthday");
-export async function createNewBirthday(data) {
-    await addDoc(birthdayRef, data);
-}
